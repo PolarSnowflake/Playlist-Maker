c3abef34dfbfdee39304a9ce2e3047540d1cfda6
PolarSnowflake <a.mayorshina@yandex.ru>
2024-06-03 00:46:41 +0300

    Спринт №8 2/3

--- app/src/main/java/com/example/playlist_maker/MainActivity.kt
+++ app/src/main/java/com/example/playlist_maker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlist_maker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +17,27 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+
+            val button1: Button = findViewById(R.id.button1)
+            val button2: Button = findViewById(R.id.button2)
+            val button3: Button = findViewById(R.id.button3)
+
+            //  реализация анонимного класса
+            button1.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    Toast.makeText(this@MainActivity, "Нажата кнопка Поиск", Toast.LENGTH_SHORT).show()
+                }
+            })
+            //  реализация лямбда-выражения
+            button2.setOnClickListener {
+                Toast.makeText(this, "Нажата кнопка  Медиатека", Toast.LENGTH_SHORT).show()
+            }
+
+            button3.setOnClickListener {
+                Toast.makeText(this, "Нажата кнопка Настройки", Toast.LENGTH_SHORT).show()
+            }
+
             insets
         }
     }

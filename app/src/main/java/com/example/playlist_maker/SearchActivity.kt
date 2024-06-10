331f8730e448b04f09f760593863002484ff4448
PolarSnowflake <a.mayorshina@yandex.ru>
2024-06-10 10:32:30 +0300

    Спринт №9 3/4

--- app/src/main/java/com/example/playlist_maker/SearchActivity.kt
+++ app/src/main/java/com/example/playlist_maker/SearchActivity.kt
@@ -1,10 +1,43 @@
 package com.example.playlist_maker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var searchBar: EditText
+    private lateinit var clearButton: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-    }}
+
+        searchBar = findViewById(R.id.searchBar)
+        clearButton = findViewById(R.id.clear_button)
+
+        searchBar.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = if (s.isNullOrEmpty()) View.GONE else View.VISIBLE
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        clearButton.setOnClickListener {
+            searchBar.text.clear()
+        }
+
+        val backButton: Button = findViewById(R.id.button_back)
+        backButton.setOnClickListener {
+            finish()
+        }
+    }
+}

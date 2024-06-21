c680b30bd6898e9297a7e016fe6d333447fb1c94
PolarSnowflake <a.mayorshina@yandex.ru>
2024-06-21 20:26:34 +0300

    Спринт №12 1/2

--- app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlist_maker/SettingsActivity.kt
@@ -1,44 +1,25 @@
 package com.example.playlist_maker
 
 import android.content.Intent
-import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
+import com.google.android.material.switchmaterial.SwitchMaterial
 
 class SettingsActivity : AppCompatActivity() {
 
-    private lateinit var sharedPreferences: SharedPreferences
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        sharedPreferences = getSharedPreferences("ThemePrefs", MODE_PRIVATE)
-        val isNightMode = sharedPreferences.getBoolean("NIGHT_MODE", false)
-        if (isNightMode) {
-            setTheme(R.style.Theme_PlaylistMaker_Dark)
-        } else {
-            setTheme(R.style.Theme_PlaylistMaker_Light)
-        }
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val switch: Switch = findViewById(R.id.switch1)
-        switch.isChecked = isNightMode
+        val themeSwitcher = findViewById<SwitchMaterial>(R.id.themeSwitcher)
+        val app = applicationContext as App
 
-        switch.setOnCheckedChangeListener { _, isChecked ->
-            sharedPreferences.edit().putBoolean("NIGHT_MODE", isChecked).apply()
-            if (isChecked) {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-                setTheme(R.style.Theme_PlaylistMaker_Dark)
-            } else {
-                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                setTheme(R.style.Theme_PlaylistMaker_Light)
-            }
-            recreate()  // перезапускаем активность
+        themeSwitcher.isChecked = app.darkTheme
+        themeSwitcher.setOnCheckedChangeListener { switcher, checked ->
+            app.switchTheme(checked)
         }
 
         val backButton: Button = findViewById(R.id.button_back)
@@ -47,8 +28,6 @@ class SettingsActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-
-
     }
 
     // Метод для шеринга приложения
@@ -77,7 +56,8 @@ class SettingsActivity : AppCompatActivity() {
 
     // Метод для открытия пользовательского соглашения
     fun openUserAgreement(view: View) {
-        val userAgreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_url)))
+        val userAgreementIntent =
+            Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_url)))
         startActivity(userAgreementIntent)
     }
 }
